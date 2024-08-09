6515e068cd91fa6b8e6e1f73631602d77cca615d
mahabaleshwars <mahabaleshwar.s@infosys.com>
2024-08-09 13:17:53 +0530

    initial commit

--- src/distributions/distribution-factory.ts
+++ src/distributions/distribution-factory.ts
@@ -10,6 +10,7 @@ import {SemeruDistribution} from './semeru/installer';
 import {CorrettoDistribution} from './corretto/installer';
 import {OracleDistribution} from './oracle/installer';
 import {DragonwellDistribution} from './dragonwell/installer';
+import {GraalVMDistribution} from './graalvm/installer';
 
 enum JavaDistribution {
   Adopt = 'adopt',
@@ -23,7 +24,8 @@ enum JavaDistribution {
   Semeru = 'semeru',
   Corretto = 'corretto',
   Oracle = 'oracle',
-  Dragonwell = 'dragonwell'
+  Dragonwell = 'dragonwell',
+  GraalVM = 'graalvm'
 }
 
 export function getJavaDistribution(
@@ -64,6 +66,8 @@ export function getJavaDistribution(
       return new OracleDistribution(installerOptions);
     case JavaDistribution.Dragonwell:
       return new DragonwellDistribution(installerOptions);
+    case JavaDistribution.GraalVM:
+      return new GraalVMDistribution(installerOptions);
     default:
       return null;
   }
