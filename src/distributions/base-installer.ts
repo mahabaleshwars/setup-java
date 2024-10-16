5cc69d6749dbad55643f7ee8a541fb6f7ee6d544
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2024-10-16 14:02:59 +0530

    code change test

--- src/distributions/base-installer.ts
+++ src/distributions/base-installer.ts
@@ -173,12 +173,12 @@ export abstract class JavaBase {
     core.setOutput('path', toolPath);
     core.setOutput('version', version);
     core.exportVariable(
-      `JAVA_HOME_${majorVersion}_${this.architecture.toUpperCase()}`,
+      `JAVA_HOME_${majorVersion}_${this.distributionArchitecture().toUpperCase()}`,
       toolPath
     );
     core.info(
       'assigned toolpath: ' +
-        `JAVA_HOME_${majorVersion}_${this.architecture.toUpperCase()}`
+        `JAVA_HOME_${majorVersion}_${this.distributionArchitecture().toUpperCase()}`
     );
   }
 
