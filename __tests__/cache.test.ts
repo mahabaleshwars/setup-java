cd54acc1e8baf9156d4cc81ddac16f543e58b4d9
mahabaleshwars <147705296+mahabaleshwars@users.noreply.github.com>
2026-06-18 18:19:01 +0530

    fix: use recursive glob for maven-wrapper.properties pattern

--- __tests__/cache.test.ts
+++ __tests__/cache.test.ts
@@ -100,7 +100,7 @@ describe('dependency cache', () => {
         await expect(restore('maven', '')).rejects.toThrow(
           `No file in ${projectRoot(
             workspace
-          )} matched to [**/pom.xml,.mvn/wrapper/maven-wrapper.properties], make sure you have checked out the target repository`
+          )} matched to [**/pom.xml,**/.mvn/wrapper/maven-wrapper.properties], make sure you have checked out the target repository`
         );
       });
       it('downloads cache based on pom.xml', async () => {
@@ -109,7 +109,7 @@ describe('dependency cache', () => {
         await restore('maven', '');
         expect(spyCacheRestore).toHaveBeenCalled();
         expect(spyGlobHashFiles).toHaveBeenCalledWith(
-          '**/pom.xml\n.mvn/wrapper/maven-wrapper.properties'
+          '**/pom.xml\n**/.mvn/wrapper/maven-wrapper.properties'
         );
         expect(spyWarning).not.toHaveBeenCalled();
         expect(spyInfo).toHaveBeenCalledWith('maven cache is not found');
@@ -124,7 +124,7 @@ describe('dependency cache', () => {
         await restore('maven', '');
         expect(spyCacheRestore).toHaveBeenCalled();
         expect(spyGlobHashFiles).toHaveBeenCalledWith(
-          '**/pom.xml\n.mvn/wrapper/maven-wrapper.properties'
+          '**/pom.xml\n**/.mvn/wrapper/maven-wrapper.properties'
         );
         expect(spyWarning).not.toHaveBeenCalled();
         expect(spyInfo).toHaveBeenCalledWith('maven cache is not found');
